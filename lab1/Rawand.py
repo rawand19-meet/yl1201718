1c30536f8ba3104749056545cc58b94ead760602
sivan18-meet <sbmislovely@gmail.com>
2017-11-01 18:57:52 +0200

    i dont know what im doing

---
+++ lab1/Rawand.py
@@ -0,0 +1,36 @@
+import turtle
+'''
+print("rawand")*100
+number1=44
+print(number1)
+number2=(number1)
+number2=number1/2
+print(number2)
+list1=[4,8,2]
+count=0
+for number in list1:
+  print (number)
+  count+=number
+print(count)
+turtle.penup()
+turtle.begin_fill()
+turtle.goto(100,50)
+turtle.pendown()
+turtle.goto(100,100)
+turtle.goto(150,100)
+turtle.goto(150,50)
+turtle.goto(100,50)
+turtle.end_fill()
+'''
+radius=80
+turtle.penup()
+
+turtle.goto(100,100)
+turtle.pendown()
+turtle.circle(radius)
+turtle.goto(150,150)
+
+
+turtle.mainloop()
+
+
---
+++ lab2/lab2.py
@@ -0,0 +1,25 @@
+'''
+smth=input("input number")
+a=[1,1,2,3,5,8,13,21,34,55,89]
+e=[]
+for i in a:
+	if i < smth:
+		e.append(i)
+print(e)
+
+'''
+
+a=[1,1,2,3,5,8,13,21,34,55,89]
+b=[1,2,3,4,5,6,7,8,9,10,11,12,13]
+c=[]
+for i in a:
+    for r in b:
+    
+	   if i==r:
+	   	if i is c:
+	   		pass
+	   	else:
+	   	    c.append(i)
+
+print(c)
+
