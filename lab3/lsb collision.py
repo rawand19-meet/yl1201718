ac4cc0e245590b50b7378e3aeaad998bfd6e7dae
sivan18-meet <sbmislovely@gmail.com>
2018-01-10 20:06:56 +0200

    collision

--- lab3/lsb collision.py
+++ lab3/lsb collision.py
@@ -13,12 +13,26 @@ Ball2 = Ball (30,"RED",40)
 Ball1.goto(100,100)
 Ball2.goto(-100,100)
 
-def check_collision(ball1,ball2):ball2.xcor()
-
+def check_collision(ball1,ball2):
 	r1=ball1.radius
 	r2=ball2.radius
 	x1=ball1.xcor()
 	x2=ball2.xcor()
-	
+	y1=ball1.ycor()
+	y2=ball2.ycor()
+	d=((((x2-x1)**2))+((y2 - y1)**2))**(1/2)
+	print(d)
+	print(r1+r2)
+	if(d < r1+r2):
+		return True 
+	else:
+		return False
+if (check_collision(Ball1,Ball2)) == True:
+		print("colide")
+else:
+		pront("no colide")
+
+
+
 mainloop()
 
