1289b73115d6c2cb6dcf7884d2ee1469928677bb
sivan18-meet <sbmislovely@gmail.com>
2018-01-22 20:12:59 +0200

    thebest

--- lab3/agario.py
+++ lab3/agario.py
@@ -2,7 +2,32 @@ from turtle import *
 import turtle
 import time
 import random
-import ball.py
+from ball import ball
+tracer(0)
+hideturtle()
+RUNNING=True 
+SLEEP=0.0077
+SCREEN_WIDTH=turtle.getcanvas().winfo_width()/2
+SCREEN_HEIGHT=turtle.getcanvas().winfo_height()/2
+
+
+my_ball=Ball(100,0,5,10."red")
+my_ball.goto(10,10)
+NUMBER_OF_BALLS=5
+MINIMUM_BALL_RADIUS = 10
+MAXIMUM_BALL_RADIUS = 100
+MINIMUM_BALL_DX = -5
+MAXIMUM_BALL_DX = 5
+MINIMUM_BALL_DY = -5
+MAXIMUM_BALL_DY = 5
+BALLS=[]
+for i in range (NUMBER OF BALLS):
+
+
+
+
+
+
 turtle.tracer
 
-hideturtle()
+
