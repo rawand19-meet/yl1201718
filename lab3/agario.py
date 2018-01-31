b527bfeb019468bb6cddb4a44295f069326f0934
sivan18-meet <sbmislovely@gmail.com>
2018-01-31 19:58:24 +0200

    weew

--- lab3/agario.py
+++ lab3/agario.py
@@ -34,76 +34,94 @@ for i in range (NUMBER_OF_BALLS):
 def move_all_balls():
 	for ball in BALLS:
 		ball.move(SCREEN_WIDTH,SCREEN_HEIGHT)
-move_all_balls()
 
-def check_all_balls_collision(self):
-	for ball_a in (BALLS):
-		if collide (ball_a,ball_b)==True:
-			ball_a_radius=ball_a.r()
-			ball_b_radius=ball_b.r()
-			if ball_a.ball_a_radius>ball_b_radius:
-				ball_a_radius=+1
-			else:
-				ball_b_radius=+1
-	
-	while X_AXISSPEED and Y_AXISSPEED ==0:
-		X_AXISSPEED = random.randint(MINIMUM_BALL_DX , MAXIMUM_BALL_DX)
-		Y_AXISSPEED = random.randint(MINIMUM_BALL_DY , MAXIMUM_BALL_DY)
-	radius = random.randint(MINIMUM_BALL_RADIUS , MAXIMUM_BALL_RADIUS)
-	print(radius)
-	color = (random.random(),random.random() ,random.random())
-	print(color)
-		if ball_a.r > ball_b.r:
-			ball_b.r = radius
-			ball_b.x = X_COORDINATE
-			ball_b.y = Y_COORDINATE
-			ball_b.dx = X_AXISSPEED
-			ball_b.dy = Y_AXISSPEED
-			ball_b.color(color)
-			ball_b.shapesize(ball_b.r/10)
-			ball_a.r = ball_a.r+1
-			ball_a.shapesize(ball_a.r/10)
-		else:
-			ball_a.r = radius
-			ball_a.x = X_COORDINATE
-			ball_a.y = Y_COORDINATE
-			ball_a.dx = X_AXISSPEED
-			ball_a.dy = Y_AXISSPEED
-			ball_a.color(color)
-			ball_a.shapesize(ball_a.r/10)
-			ball_b.r = ball_a.r+1
-			ball_b.shapesize(ball_b.r/10)
+def check_all_balls_collision():
+	for ball_a in BALLS:
+		for ball_b in BALLS:
+			if collide (ball_a,ball_b)==True:
+				ball_a_radius=ball_a.r
+				ball_b_radius=ball_b.r
+				if ball_a_radius>ball_b_radius:
+					ball_a.r+=1
+				else:
+					ball_b.r+=1
+
+				while (X_AXISSPEED == 0 and Y_AXISSPEED ==0):
+					X_AXISSPEED = random.randint(MINIMUM_BALL_DX , MAXIMUM_BALL_DX)
+					Y_AXISSPEED = random.randint(MINIMUM_BALL_DY , MAXIMUM_BALL_DY)
+				radius = random.randint(MINIMUM_BALL_RADIUS , MAXIMUM_BALL_RADIUS)
+				print(radius)
+				color = (random.random(),random.random() ,random.random())
+				print(color)
+				if ball_a.r > ball_b.r:
+					ball_b.r = radius
+					ball_b.goto = (X_COORDINATE, Y_COORDINATE)
+					ball_b.dx = X_AXISSPEED
+					ball_b.dy = Y_AXISSPEED
+					ball_b.color(color)
+					ball_b.shapesize(ball_b.r/10)
+					ball_a.r = ball_a.r+1
+					ball_a.shapesize(ball_a.r/10)
+				else:
+					ball_a.r = radius
+					ball_a.goto=(X_COORDINATE , Y_COORDINATE)
+					ball_a.dx = X_AXISSPEED
+					ball_a.dy = Y_AXISSPEED
+					ball_a.color(color)
+					ball_a.shapesize(ball_a.r/10)
+					ball_b.r = ball_a.r+1
+					ball_b.shapesize(ball_b.r/10)
 def check_myball_collision():
 	for ball in BALLS:
 		if collide(MY_BALL,ball) == True:
-			ball_r4 = ball.random
+			ball_r4 = ball.r
 			my_ball_r4 = MY_BALL.r 
-			if my_ball_r4 and MY_BALL.shapesize() < BALL.shapesize():
-
+			if my_ball_r4 <ball_r4:
 
 				return False
+
+
+			ball.a
+
 			X_COORDINATE=random.randint(round(-SCREEN_WIDTH),round(SCREEN_WIDTH))
 			Y_COORDINATE=random.randint(round(-SCREEN_HEIGHT),round(SCREEN_HEIGHT))
 			X_AXISSPEED = random.randint(MINIMUM_BALL_DX , MAXIMUM_BALL_DX)
 			Y_AXISSPEED = random.randint(MINIMUM_BALL_DY , MAXIMUM_BALL_DY)
-			while X_AXISSPEED and  Y_AXISSPEED == 0
-			X_AXISSPEED = random.randint(MINIMUM_BALL_DX , MAXIMUM_BALL_DX)
-			Y_AXISSPEED = random.randint(MAXIMUM_BALL_DY , MAXIMUM_BALL_DY)
+			while X_AXISSPEED and  Y_AXISSPEED == 0:
+				X_AXISSPEED = random.randint(MINIMUM_BALL_DX , MAXIMUM_BALL_DX)
+				Y_AXISSPEED = random.randint(MAXIMUM_BALL_DY , MAXIMUM_BALL_DY)
 
 			radius = random.randint(MINIMUM_BALL_RADIUS , MAXIMUM_BALL_RADIUS)
 			print(radius)
 			color = (random.random(),random.random(),random.random())
 			print(color)
-	while True:
-		for ball in BALLS:
-			ball.move(SCREEN_WIDTH,SCREEN_HEIGHT)
-	def collide (ball_a,ball_b):
-		if ball_a==ball_b:
-			return False
-		if (math.pow(ball_a.x_ball_b.x))+(math.pow(ball_a.y-ball_b.y))):
-	    else:
-	    	return False
-turtle.getscreen().update()
+			ball.r = radius
+			ball.goto = (X_COORDINATE, Y_COORDINATE)
+			ball.dx = X_AXISSPEED
+			ball.dy = Y_AXISSPEED
+			ball.color(color)
+			ball.shapesize(ball_b.r/10)
+			MY_BALL.r = ball_a.r+1
+			MY_BALL.shapesize(ball_a.r/10)
+
+		#while True:
+			#for ball in BALLS:
+			#	ball.move(SCREEN_WIDTH,SCREEN_HEIGHT)
+def collide (ball_a,ball_b):
+	if ball_a==ball_b:
+		return False
+	#3**2
+
+	d =((ball_b.xcor-balll_a.xcor)**2+(ball_b.ycor-ball_a.ycor)**2)**0.5
+	r=ball_a.r+ ball_b.r
+	   	
+	if d+10 <= r:
+		return True
+	else:
+		return False
+
+ 	hideturtle()
+ 	turtle.getscreen().update()
 mainloop()
 
 
